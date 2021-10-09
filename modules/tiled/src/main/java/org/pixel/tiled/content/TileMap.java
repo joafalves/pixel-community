f4ac6b82f2054cee42a712e81ee2befdd651b64f
Nuno Alves <nyansword@gmail.com>
2021-10-09 21:14:30 +0100

    add: TileMap class and Importer class

---
+++ modules/tiled/src/main/java/org/pixel/tiled/content/TileMap.java
@@ -0,0 +1,15 @@
+/*
+ * This software is available under Apache License
+ * Copyright (c) 2020
+ */
+
+package org.pixel.tiled.content;
+
+import org.pixel.commons.lifecycle.Disposable;
+
+public class TileMap implements Disposable {
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ modules/tiled/src/main/java/org/pixel/tiled/content/importer/TileMapImporter.java
@@ -0,0 +1,17 @@
+/*
+ * This software is available under Apache License
+ * Copyright (c) 2020
+ */
+
+package org.pixel.tiled.content.importer;
+
+import org.pixel.content.ContentImporter;
+import org.pixel.content.ImportContext;
+import org.pixel.tiled.content.TileMap;
+
+public class TileMapImporter implements ContentImporter<TileMap> {
+    @Override
+    public TileMap process(ImportContext ctx) {
+        return null;
+    }
+}
