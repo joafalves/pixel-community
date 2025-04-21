16b6d2cfa585b28dcde9aa4831c4bd0113348998
jofalves <joao.cpp.sca@gmail.com>
2025-04-21 19:34:18 +0100

    fix: added missing builder defaults (lombok) update: gradlew wrapper 8.12.1 > 8.13

--- modules/content/src/main/java/org/pixel/content/importer/settings/FontImporterSettings.java
+++ modules/content/src/main/java/org/pixel/content/importer/settings/FontImporterSettings.java
@@ -10,8 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FontImporterSettings implements ContentImporterSettings {
+    @Builder.Default
     private int fontSize = 24;
+    @Builder.Default
     private int horizontalSpacing = 0;
+    @Builder.Default
     private int verticalSpacing = 0;
+    @Builder.Default
     private int oversampling = 1;
 }
